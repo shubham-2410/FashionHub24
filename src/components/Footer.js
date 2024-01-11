45ed508cb71191d7f945365da918b06250772d4a
Shubham Phalke <onlyphalke143@gmail.com>
2024-01-11 17:41:22 +0530

    FashionHub24

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=' flex w-full bg-[#010203] items-center justify-center text-white h-20 font-bold lg:text-2xl sm:text-xl'>
-        <span>Made with ❤️ @FashonHub24</span>
+        <span>Made with ❤️ @FashionHub24</span>
     </div>
   )
 }
